188ef50224bcd17d406ba4d603a4491a1c9a8511
mlmedina92 <lm30540@gmail.com>
2024-04-05 13:26:08 -0300

    mailling

---
+++ mailer.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+function sendEmail()
+{
+    require 'PHPMailer/PHPMailer.php';
+
+    // Recibir datos del formulario
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+
+    // Configurar la instancia de PHPMailer
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->Host = 'mail.smtp2go.com';  // Host del servidor SMTP
+    $mail->SMTPAuth = true;                 // Habilitar autenticación SMTP
+    $mail->Username = 'webform@camilocapital.es'; // Tu dirección de correo electrónico
+    $mail->Password = 'NFu6J9OSLYgzh8Ot';       // Tu contraseña de correo electrónico
+    $mail->SMTPSecure = 'tls';               // Habilitar cifrado TLS
+    $mail->Port = 2525;                       // Puerto SMTP
+
+    // Configurar el contenido del correo electrónico
+    $mail->setFrom($email, $nombre);
+    $mail->addAddress('info@camilocapital.es', 'Camilo Capital');
+    $mail->Subject = $asunto;
+    $mail->Body = $mensaje;
+
+    // Procesar el envío del correo electrónico
+    if (!$mail->send()) {
+        echo 'El mensaje no pudo ser enviado.';
+        echo 'Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'El mensaje ha sido enviado exitosamente.';
+    }
+}
+?>
